c558e3d3ca1224f8a78aa53f0056568a790a6cd6
Andreas Pelme <andreas@pelme.se>
2015-06-15 09:23:02 +0200

    Use github https:// instead of git:// in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd
-git clone git://github.com/pelme/pelmerc.git .pelmerc
+git clone https://github.com/pelme/pelmerc.git .pelmerc
 cd .pelmerc
 git submodule update --init
 ~/.pelmerc/bin/prc_install
