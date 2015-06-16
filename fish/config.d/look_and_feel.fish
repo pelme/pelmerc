854c7e702bd0929432762a0fa5c7af3162496bb7
Andreas Pelme <andreas@pelme.se>
2015-06-16 22:42:00 +0200

    Decent prompt

--- fish/config.d/look_and_feel.fish
+++ fish/config.d/look_and_feel.fish
@@ -2,38 +2,30 @@ function prompt_long_pwd --description 'Print the current working directory'
     echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||'
 end
 
+set -g __fish_git_prompt_color_branch green bold
 
-function fish_prompt --description 'Write out the prompt'
-	# Just calculate these once, to save a few cycles when displaying the prompt
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
+function fish_prompt --description 'Write out the prompt, Andreas style'
 
 	if not set -q __fish_prompt_normal
 		set -g __fish_prompt_normal (set_color normal)
 	end
 
-	switch $USER
+    if not set -q __fish_prompt_hostname
+        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    end
 
-		case root
+    if set -q VIRTUAL_ENV
+        echo -n -s (set_color cyan) "[" "$__fish_prompt_normal"
+        echo -n -s (set_color -o cyan) (basename "$VIRTUAL_ENV") "$__fish_prompt_normal"
+        echo -n -s (set_color cyan) "] " "$__fish_prompt_normal"
+    end
 
-		if not set -q __fish_prompt_cwd
-			if set -q fish_color_cwd_root
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-			else
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-			end
-		end
+    if not set -q IS_LOCAL
+        echo -n -s "$USER" @ "$__fish_prompt_hostname" ' '
+    end
 
-		echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_long_pwd) "$__fish_prompt_normal" '# '
-
-		case '*'
-
-		if not set -q __fish_prompt_cwd
-			set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-		end
-
-		echo -n -s "$USER" @ "$__fish_prompt_hostname" ' ' "$__fish_prompt_cwd" (prompt_long_pwd) "$__fish_prompt_normal" '> '
-
-	end
+    echo -n -s (__fish_git_prompt (set_color green)"[%s"(set_color green)"] ")
+    echo -n -s (set_color normal) (prompt_long_pwd) "$__fish_prompt_normal"
+    echo -n -s (set_color -o purple ) ' ➤  ' "$__fish_prompt_normal"
+    echo -n -s "$__fish_prompt_normal"
 end
