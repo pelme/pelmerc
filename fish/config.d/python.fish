a560e1e437f971833147a9945359a48ee0c088f9
Andreas Pelme <andreas@pelme.se>
2015-06-16 22:41:44 +0200

    virtualfish

--- fish/config.d/python.fish
+++ fish/config.d/python.fish
@@ -2,3 +2,5 @@ set --export WORKON_HOME $HOME/.virtualenvs
 set --export PYTHONIOENCODING "UTF-8"
 set --export VIRTUAL_ENV_DISABLE_PROMPT y
 set --export FLAKE8_STRICT t
+
+source ~/.pelmerc/fish/external/virtualfish/virtualfish/virtual.fish
