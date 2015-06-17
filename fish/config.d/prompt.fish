17f777644ede9ed970cb52c1c2ff7c686d071a14
Andreas Pelme <andreas@pelme.se>
2015-06-17 20:49:14 +0200

    Prompt cosmetica

--- fish/config.d/prompt.fish
+++ fish/config.d/prompt.fish
@@ -2,7 +2,14 @@ function prompt_long_pwd --description 'Print the current working directory'
     echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||'
 end
 
-set -g __fish_git_prompt_color_branch green bold
+
+set -g __pelme_start_char ‹
+set -g __pelme_end_char ›
+set -g __pelme_prompt_symbol →
+set -g __pelme_git_color green
+set -g __pelme_virtualenv_color cyan
+
+set -g __fish_git_prompt_color_branch $__pelme_git_color bold
 
 function fish_prompt --description 'Write out the prompt, Andreas style'
 
@@ -15,17 +22,16 @@ function fish_prompt --description 'Write out the prompt, Andreas style'
     end
 
     if set -q VIRTUAL_ENV
-        echo -n -s (set_color cyan) "[" "$__fish_prompt_normal"
-        echo -n -s (set_color -o cyan) (basename "$VIRTUAL_ENV") "$__fish_prompt_normal"
-        echo -n -s (set_color cyan) "] " "$__fish_prompt_normal"
+        echo -n -s (set_color $__pelme_virtualenv_color) $__pelme_start_char "$__fish_prompt_normal"
+        echo -n -s (set_color -o $__pelme_virtualenv_color) (basename "$VIRTUAL_ENV") "$__fish_prompt_normal"
+        echo -n -s (set_color $__pelme_virtualenv_color) $__pelme_end_char " $__fish_prompt_normal"
     end
 
     if not set -q IS_LOCAL
         echo -n -s "$USER" @ "$__fish_prompt_hostname" ' '
     end
 
-    echo -n -s (__fish_git_prompt (set_color green)"[%s"(set_color green)"] ")
+    echo -n -s (__fish_git_prompt (set_color $__pelme_git_color)$__pelme_start_char"%s"(set_color $__pelme_git_color)$__pelme_end_char" ")
     echo -n -s (set_color normal) (prompt_long_pwd) "$__fish_prompt_normal"
-    echo -n -s (set_color -o purple ) ' ➤  ' "$__fish_prompt_normal"
-    echo -n -s "$__fish_prompt_normal"
+    echo -n -s (set_color -o purple ) " " $__pelme_prompt_symbol " $__fish_prompt_normal"
 end
