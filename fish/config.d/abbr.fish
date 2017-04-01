c90e662da88a6d32468c5551043d4640ab30e8c9
Andreas Pelme <andreas@pelme.se>
2017-04-01 21:29:22 +0200

    Updated abbrs

--- fish/config.d/abbr.fish
+++ fish/config.d/abbr.fish
@@ -1,13 +1,22 @@
 abbr --add g git
+abbr --add gs git status
+abbr --add gc git commit
 abbr --add gd git diff
+abbr --add gl git log
 abbr --add gds git diff --staged
-abbr --add gst git status
 abbr --add gco git checkout
-abbr --add gci git commit
 abbr --add gpr git pull --rebase
-abbr --add grb git rebase -i HEAD~5
+abbr --add grb git rebase
+abbr --add gp git push
+abbr --add gpf git push --force-with-lease
+abbr --add grbi git rebase -i HEAD~5
+abbr --add grbm git rebase master
 abbr --add gbr git branch
 abbr --add gaa git add --all
+abbr --add grs git reset
+abbr --add grm git rm
+abbr --add grsh git reset HEAD^
+abbr --add wip git add --all \; git commit -m WIP
 
 abbr --add dj django-admin
 abbr --add djr django-admin runserver
