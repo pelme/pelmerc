6b5fad7613aff98da3a68aa1842d960375e96a72
Andreas Pelme <andreas@pelme.se>
2019-03-28 10:41:55 +0100

    Prompt updates.

--- fish/config.d/prompt.fish
+++ fish/config.d/prompt.fish
@@ -3,11 +3,12 @@ function prompt_long_pwd --description 'Print the current working directory'
 end
 
 
-set -g __pelme_start_char ‹
-set -g __pelme_end_char ›
-set -g __pelme_prompt_symbol →
+set -g __pelme_start_char ""
+set -g __pelme_end_char ""
+set -g __pelme_prompt_symbol "\$"
 set -g __pelme_git_color green
 set -g __pelme_virtualenv_color cyan
+set -g __pelme_pipenv_color yellow
 
 set -g __fish_git_prompt_color_branch $__pelme_git_color --bold
 
@@ -21,7 +22,11 @@ function fish_prompt --description 'Write out the prompt, Andreas style'
         set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
     end
 
-    if set -q VIRTUAL_ENV
+    if set -q PIPENV_ACTIVE
+        echo -n -s (set_color $__pelme_pipenv_color) $__pelme_start_char "$__fish_prompt_normal"
+        echo -n -s (set_color -o $__pelme_pipenv_color) pipenv  "$__fish_prompt_normal"
+        echo -n -s (set_color $__pelme_pipenv_color) $__pelme_end_char " $__fish_prompt_normal"
+    else if set -q VIRTUAL_ENV
         echo -n -s (set_color $__pelme_virtualenv_color) $__pelme_start_char "$__fish_prompt_normal"
         echo -n -s (set_color -o $__pelme_virtualenv_color) (basename "$VIRTUAL_ENV") "$__fish_prompt_normal"
         echo -n -s (set_color $__pelme_virtualenv_color) $__pelme_end_char " $__fish_prompt_normal"
