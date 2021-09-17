00c27515bced9d2cb99ab160c4d98a761caad98a
Andreas Pelme <andreas@pelme.se>
2021-09-17 20:22:30 +0200

    Remove virtualfish

--- fish/conf.d/python.fish
+++ fish/conf.d/python.fish
@@ -2,8 +2,6 @@ set --export PYTHONIOENCODING "UTF-8"
 set --export VIRTUAL_ENV_DISABLE_PROMPT y
 set --export FLAKE8_STRICT t
 
-source ~/.pelmerc/fish/external/virtualfish/virtualfish/virtual.fish
-
 # status --is-interactive; and source (pyenv init -|psub)
 # status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
