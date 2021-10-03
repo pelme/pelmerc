a44d0efb64c0b5a1a0008e09641bda086112393a
Andreas Pelme <andreas@pelme.se>
2021-10-03 20:46:21 +0200

    Configure pyenv properly.

--- fish/conf.d/python.fish
+++ fish/conf.d/python.fish
@@ -2,7 +2,5 @@ set --export PYTHONIOENCODING "UTF-8"
 set --export VIRTUAL_ENV_DISABLE_PROMPT y
 set --export FLAKE8_STRICT t
 
-# status --is-interactive; and source (pyenv init -|psub)
-# status --is-interactive; and source (pyenv virtualenv-init -|psub)
-
-pyenv init - | source
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
