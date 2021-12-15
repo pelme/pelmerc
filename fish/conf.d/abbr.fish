9333e51865e24c78e34b653ce2cd24ed19658e78
Andreas Pelme <andreas@pelme.se>
2021-12-15 13:22:33 +0100

    Misc changes

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -5,17 +5,19 @@ abbr --add gd git diff
 abbr --add gl git log
 abbr --add gds git diff --staged
 abbr --add gsw git switch
-abbr --add gpr git pull --rebase
 abbr --add grb git rebase
+abbr --add grbi git rebase -i origin/master
 abbr --add gp git push
 abbr --add gpf git push --force-with-lease
-abbr --add gri git rebase -i origin/master
 abbr --add gpm git pull --rebase origin master
+abbr --add gpr git pull --rebase
 abbr --add glm git log --oneline --reverse origin/master..
 abbr --add gbr git branch
 abbr --add gdm git diff origin/master
 abbr --add gaa git add --all
-abbr --add gr git restore
+abbr --add gam git commit -a --amend
+abbr --add gre git restore
+
 abbr --add grs git reset
 abbr --add grsh git reset HEAD^
 abbr --add wip git add --all \; git commit -m WIP
